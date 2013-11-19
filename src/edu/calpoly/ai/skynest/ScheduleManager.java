a8d285fb291c688fe200b9db67335f312a8d9587
bivy <g0tmk@pcp072536pcs.wireless.calpoly.edu>
2013-11-19 10:12:40 -0800

    moved scheduleUpdater code into scheduleManager, added more comments

--- src/edu/calpoly/ai/skynest/ScheduleManager.java
+++ src/edu/calpoly/ai/skynest/ScheduleManager.java
@@ -3,6 +3,7 @@ package edu.calpoly.ai.skynest;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
+
 /**ScheduleManager: Used to initialize the schedule (set schedule values to a 
  * 					certain time, regardless of the averaging algorithm) and 
  * 					read the current values from the schedule.
@@ -36,6 +37,7 @@ import android.content.SharedPreferences.Editor;
  * System.out.println("Mon Arrive:" + sm.getTimeSlot(3));
  * 
  **/
+
 public class ScheduleManager {
 	protected Schedule loaded_schedule;
 	SharedPreferences sp;
@@ -43,19 +45,26 @@ public class ScheduleManager {
 	/** constants **/
 	private int NUM_DAYS = 7;
 	private int NUM_TIMES = 2;
+	double NEW_DATA_WEIGHT = 0.20;	//new data weighs 20% when averaged w/ old data
 
 	public ScheduleManager(SharedPreferences sp) {
 		this.sp = sp;
 	}
 
-	/** returns whole schedule from shared preferences as a Schedule object **/
+	/** returns whole schedule from shared preferences as a Schedule object 
+	 * 	
+	 * 	the private Schedule class inside the ScheduleManager class might 
+	 * 	need to be changed to protected or something for to make the schedule 
+	 * 	object visible 
+	 **/
 	public Schedule getSchedule() {
 		loadSchedule();
 		return loaded_schedule;
 	}
 
 	/** returns whole schedule as an array of ints 
-	 * 	returns: int[] [sun_depart, sun_arrive, mon_depart, ... **/
+	 * 	returns: int[14] [sun_depart, sun_arrive, mon_depart, ... 
+	 **/
 	public int[] getScheduleInts() {
 		int[] int_schedule = new int[NUM_DAYS*2];
 		int dayInt, timeInt;
@@ -77,7 +86,8 @@ public class ScheduleManager {
 	/** returns specific integer time from the schedule
 	 *  parameters: int dayInt (0-6 : sun, mon, tue, ...)
 	 *  			int timeInt (0-1 : departure, arrival) 
-	 *  returns: 	int (minutes since start of day) **/
+	 *  returns: 	int (minutes since start of day) 
+	 **/
 	public int getTime(int dayInt, int timeInt) {
 		loadSchedule();
 		return loaded_schedule.getTime(dayInt, timeInt);
@@ -85,7 +95,8 @@ public class ScheduleManager {
 	
 	/** returns specific integer time from the schedule
 	 *  parameter: 	int slot (0-13 : sun_depart, sun_arrive, mon_depart, ...)
-	 *  returns: 	int (minutes since start of day) **/
+	 *  returns: 	int (minutes since start of day) 
+	 **/
 	public int getTimeSlot(int slot) {
 		return this.getTime(slot/2, slot%2);
 	}
@@ -93,7 +104,8 @@ public class ScheduleManager {
 	/** sets specific integer time in the schedule 
 	 *  parameters: int dayInt (0-6 : sun, mon, tue, ...)
 	 *  			int timeInt (0-1 : departure, arrival)
-	 *  			int value (0-1440 : minutes since start of day) **/
+	 *  			int value (0-1440 : minutes since start of day) 
+	 **/
 	public void setTime(int dayInt, int timeInt, int value) {
 		loadSchedule();
 		loaded_schedule.setTime(dayInt, timeInt, value);
@@ -102,11 +114,38 @@ public class ScheduleManager {
 
 	/** sets specific integer time in the schedule 
 	 *  parameters: int slot (0-13 : sun_depart, sun_arrive, mon_depart, ...)
-	 *  			int value (0-1440 : minutes since start of day) **/
+	 *  			int value (0-1440 : minutes since start of day) 
+	 **/
 	public void setTimeSlot(int slot, int value) {
 		this.setTime(slot/2, slot%2, value);
 	}
 	
+	/** updates shared preferences schedule using the time averaging algorithm.
+	 * 	uses constant NEW_DATA_WEIGHT
+	 *  parameters: int dayInt (0-6 : sun, mon, tue, ...)
+	 *  			int timeInt (0-1 : departure, arrival)
+	 *  			int value (0-1440 : minutes since start of day) 
+	 **/
+	public void updateTime(int dayInt, int timeInt, int value) {
+		int old_value, new_value;
+		
+		old_value = loaded_schedule.getTime(dayInt, timeInt);
+		new_value = (int) (old_value*(1-NEW_DATA_WEIGHT) + (value*NEW_DATA_WEIGHT));
+		
+		loaded_schedule.setTime(dayInt, timeInt, new_value);
+	}
+
+	/** updates shared preferences schedule using the time averaging algorithm.
+	 * 	uses constant NEW_DATA_WEIGHT
+	 *  parameters: int slot (0-13 : sun_depart, sun_arrive, mon_depart, ...)
+	 *  			int value (0-1440 : minutes since start of day) 
+	 **/
+	public void updateTimeSlot(int slot, int value) {
+		this.updateTime(slot/2, slot%2, value);
+	}
+	
+	
+	/* private functions */
 	/** saves the loaded schedule into shared preferences **/
 	private void saveSchedule() {
 		Editor e = sp.edit();
@@ -118,15 +157,16 @@ public class ScheduleManager {
 	
 	/** reads shared preferences to refresh the the loaded schedule **/
 	private void loadSchedule() {
+		if(loaded_schedule == null){
+			loaded_schedule = new Schedule();
+		}
     	for(int index=0; index<14; index++) {
     		if(sp != null && sp.contains("sched" + index))
     			loaded_schedule.setTime(index/2, index%2, sp.getInt("sched" + index, -1));
     	}
 	}
-	 
 	
-	
-	protected class Schedule {
+	private class Schedule {
 		private Day[] days = new Day[NUM_DAYS];
 		
 		public Schedule() {
