08c3f9b3504617f0c995c4e0dc26b66dfaa7141b
Mark Lerner <g.um104.w@gmail.com>
2013-12-03 10:19:55 -0800

    Updated the location broacast mechanism such that any time we enter or exit a radius around the set location it will properly call ScheduleManager to update the appropriate time.

--- src/edu/calpoly/ai/skynest/LocationBroadcastReceiver.java
+++ src/edu/calpoly/ai/skynest/LocationBroadcastReceiver.java
@@ -4,6 +4,10 @@
 
 package edu.calpoly.ai.skynest;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import java.util.Calendar;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,11 +19,21 @@ public class LocationBroadcastReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		String key = LocationManager.KEY_PROXIMITY_ENTERING;
 		Boolean entering = intent.getBooleanExtra(key, false);
+		ScheduleManager schedMan =
+				new ScheduleManager(context.getSharedPreferences(MainActivity.PREF_FILE, MODE_PRIVATE));
+
+		// returns constants SUNDAY through SATURDAY, equal to 1 through 7 respectively
+		Calendar cal = Calendar.getInstance();
+		int day = cal.get(Calendar.DAY_OF_WEEK) - 1; // -1 to make 0-based
+		int min = (cal.get(Calendar.HOUR_OF_DAY) * 60) + cal.get(Calendar.MINUTE);
+		
 		if (entering) {
-			// TODO: alert schedule manager to change arrival time, dependent on API
+			// alert schedule manager to change arrival time
+			schedMan.updateTime(day, 1, min);
 		}
 		else {
-			// TODO: alert schedule manager to change departure time, dependent on API
+			// alert schedule manager to change departure time
+			schedMan.updateTime(day, 0, min);
 		}
 	}	
 }	
