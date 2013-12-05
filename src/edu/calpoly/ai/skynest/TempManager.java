e11835f55d323bc692752690579ae8aa37910d11
FinancialPanther <financial0panther@gmail.com>
2013-12-04 23:59:08 -0800

    Disregard previous commit This should work

--- src/edu/calpoly/ai/skynest/TempManager.java
+++ src/edu/calpoly/ai/skynest/TempManager.java
@@ -4,7 +4,7 @@ package edu.calpoly.ai.skynest;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-
+import java.util.Calendar;
 
 public class TempManager {
 	
@@ -12,11 +12,13 @@ public class TempManager {
 	private static final String PREFERED_TEMP = "PREFERED_TEMP";
 	private static final double TEMP_NOT_SET = -100;
 	private int currentTime;
+	private ScheduleManager mySchedManager;
 	private SharedPreferences sp;
 	
 	/** Constructor makes a new mock nest every time possibly change implementation*/
 	public TempManager(SharedPreferences sp) {
 		this.sp = sp;
+		mySchedManager = new ScheduleManager(sp);
 	}
 	
 	/** Sets Preferred temp in the shared preferences*/
@@ -35,9 +37,20 @@ public class TempManager {
 			return TEMP_NOT_SET;
 		}
 	}
-	/** Should this int be a time variable */
-	public double getHouseTemp(int Time) {
+	/** returns double of the house temp
+	 *  parameters: None
+	 *  returns: 	double (temp) 
+	 **/
+	public double getHouseTemp() {
+		int Time;
+		Calendar c = Calendar.getInstance();//get Calendar
 		currentTime = (int)(System.currentTimeMillis()/60000);// call time get minutes
+		
+		/*if((Time = mySchedManager.getTime(c.get(Calendar.DAY_OF_WEEK), 0)) >= currentTime)
+		{
+			Time = mySchedManager.getTime(c.get(Calendar.DAY_OF_WEEK), 1);
+		}*/
+		Time = mySchedManager.getTime(c.get(Calendar.DAY_OF_WEEK), 1); //Always get arrival time
 		return MockThermostat.getTemp(currentTime, Time, getPreferedTemp());//get current temp
 	}
 
